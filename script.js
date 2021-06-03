927a48a6c89400db7641fc9dba1f6ca2e15519eb
Alex Fourmy <alexfourmy@gmail.com>
2021-06-03 12:57:55 +0200

    completed the display balance functionality

--- script.js
+++ script.js
@@ -74,6 +74,13 @@ const displayMovements = function(movements) {
 
 displayMovements(account1.movements)
 
+const calcDisplayBalance = function(movements){
+  const balance = movements.reduce((acc, mov) => acc + mov, 0)
+  labelBalance.textContent = `${balance} EUR`
+}
+
+calcDisplayBalance(account1.movements)
+
 const createUsername = function(accs) {
   accs.forEach(function(acc) {
     acc.username = acc.owner
@@ -87,4 +94,5 @@ const createUsername = function(accs) {
 const user = 'Steven Thomas Williams'
 
 createUsername(accounts)
-console.log(accounts)
+
+
